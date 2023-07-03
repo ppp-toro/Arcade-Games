726797373594eacf312fbe78cd7bfa591500653a
Pablo Rebollo Toro <rebollotoro@gmail.com>
2023-07-03 18:25:24 +0200

    Add backed registration
    Header and backed registration

--- Server/proyecto/App/views.py
+++ Server/proyecto/App/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'general/template.html')
+
+def registro(request):
+    return render(request, 'registration/registro.html')
+
+def login(request):
+    return render(request, 'registration/login.html')
+
